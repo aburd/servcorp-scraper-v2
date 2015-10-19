617960a102a1c173588b17dbe37a2365fec29e03
Aaron Burdick <burdick.aaron@gmail.com>
2015-10-19 23:51:31 +0900

    Some nonsense trying to use promises

--- ver2.js
+++ ver2.js
@@ -3,11 +3,12 @@
 (function(){
 	//declare necessary global variables
 	var	jbuilder = require('jbuilder'),
-			request = require('request'),
-			cheerio = require('cheerio');
+		request = require('request'),
+		cheerio = require('cheerio'),
+		Promise = require('bluebird');
 
 	var servcorpAu = 'http://www.servcorp.com.au',
-			worldwideLocations = '/en/worldwide-locations';
+		worldwideLocations = '/en/worldwide-locations';
 	var links = [];
 	var servcorp = [];
 
@@ -20,7 +21,8 @@
 	function Location(name, url){
 		this.name = name,
 		this.url = url,
-		this.location = [];
+		this.address = '',
+		this.gps = [];
 	}
 
 	// 1Go to webpage
@@ -41,44 +43,17 @@
 				});
 
 			}
-			console.log(servcorp);
+			//console.log(servcorp);
 			
 			callback();
 		});
 	}
 
-	goToWebpage( function(){
-		//2go to each country link
-
-		servcorp.forEach(function(country, i){
-			getInfo(country, country.url, [['strong','p.building-name'], ['a:first-child', 'div.building-call-to-action']], function(countryLocations) {
-				//servcorp[i].locations.push(countryLocations);			
-				pusher(servcorp[i].locations, countryLocations, function(){
-					servcorp.forEach(function( country, i ){
-						console.log('The country is: ', country.name, 'and the locations are: ', country.locations);
-					});
-				})
-			});
-		});				
-
-	});
-
-
 	function pusher( pushee, data, callback ) {
 		var res = pushee.push(data);
 
-		callback();
+		callback && callback();
 	}
-	
-	
-	
-	// get location links and names
-	
-	// 3go to each location
-	
-	// get gps and address
-	
-	// 4Log the information
 
 	// this function goes to multiple urls and gets information
 	function getInfo (country, url, selector, callback) {
@@ -111,4 +86,137 @@
 			callback && callback(locations);
 		}); 
 	}
+	// this function goes to multiple urls and gets information
+	function getInfo2 (country, url, selector, callback) {
+	  
+	  var res = [];
+	  var res2 = [];
+	  var locations = [];
+
+		request(url, function(error, response, body){
+			if(!error && response.statusCode == 200){
+				var $ = cheerio.load(body);
+				//name
+	   			$(selector[0][0], selector[0][1]).each(function( index, element ){
+		   			res = $(element).html().replace(/\s{2,}/g, '');
+				  });
+
+	   			//url
+				$(selector[1][0], selector[1][1]).each( function( index, element){
+					res2 = $(element).html().match(/\d{1,3}.\d{4,}/g);
+				});
+  				
+			} else {
+				console.log('there was an error with: ', url)
+				//throw error;
+			}
+			country.address = res;
+			country.gps = res2;
+
+			callback && callback(res);
+		}); 
+	}
+
+	function getInfoNew (countryLocations, selector, callback) {
+	
+		return new Promise(function( resolve, reject ){
+			var res = [];
+			var res2 = [];
+			var locations = [];
+
+		    countryLocations.forEach( function( countryLocation, i, countryLocations ){
+
+				request(countryLocation.url, function(error, response, body){
+					if(!error && response.statusCode == 200){
+						var $ = cheerio.load(body);
+						//name
+			   			$(selector[0][0], selector[0][1]).each(function( index, element ){
+				   			res = $(element).html().replace(/\s{2,}/g, '');
+						  });
+
+			   			//url
+						$(selector[1][0], selector[1][1]).each( function( index, element){
+							res2 = $(element).html().match(/\d{1,3}.\d{4,}/g);
+						});
+		  				
+					} else {
+						//console.log('there was an error with: ', countryLocation.url)
+						//throw error;
+						countryLocation.address = "not found";
+						countryLocation.gps = [0, 0];
+						reject(error);
+					}
+					countryLocation.address = res;
+					countryLocation.gps = res2;
+
+					//callback && callback(res);
+					resolve(countryLocations);
+				}); 
+		    } );		
+		
+		});
+
+	}
+
+	function fillThoseObjects( ){
+
+		servcorp.forEach(function(country, i){
+			// 3. get info about each country (name and links) and put that into the object
+			getInfo(country, country.url, [['strong','p.building-name'], ['a:first-child', 'div.building-call-to-action']], function(countryLocations) {
+				// 4. Put the results in the object			
+				pusher(servcorp[i].locations, countryLocations);
+
+				// 5. go to each location
+				// get gps and address
+				var filledCountries = getInfoNew(countryLocations, [['p','.location-info address'], ['script','.module.building-location-map .column-container']] );
+				Promise.settle(filledCountries).then( function(results){
+					console.log('done');
+					console.log(results);
+					allPromiseResults.push(results);
+				});
+
+			});
+		});
+		
+	}
+
+	// 1. go to main page and put all data in the servcorp object
+	goToWebpage( function(){
+		// 2. go to each country link in the servcorp object
+		var filledObjects = fillThoseObjects();
+		Promise.settle(filledObjects).then( function(results) {
+			console.log('All done.');
+		});
+
+		//servcorp.forEach(function(country, i){
+			// 3. get info about each country (name and links) and put that into the object
+		//	getInfo(country, country.url, [['strong','p.building-name'], ['a:first-child', 'div.building-call-to-action']], function(countryLocations) {
+				// 4. Put the results in the object			
+		//		pusher(servcorp[i].locations, countryLocations);
+
+				// 5. go to each location
+				// get gps and address
+				// var filledCountries = getInfoNew(countryLocations, [['p','.location-info address'], ['script','.module.building-location-map .column-container']] );
+				// Promise.settle(filledObjects).then( function(results){
+				// 	console.log('done');
+				// 	allPromiseResults.push(results);
+				// });
+
+				// getInfoNew(countryLocations, [['p','.location-info address'], ['script','.module.building-location-map .column-container']], function(res){
+				// 	console.log('done');
+				// });
+
+				// countryLocations.forEach( function( countryLocation, i, locations ){
+				// 	getInfo2( countryLocation, countryLocation.url, [['p','.location-info address'], ['script','.module.building-location-map .column-container']], function( address ){
+				// 		console.log(countryLocation);
+				// 	} );
+				// } );
+
+				// 6. Log the information
+
+		//	});
+
+		//});
+
+	});
 })();
